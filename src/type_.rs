6f1001af7b23de2d1a61dce9dca3d31ecb5110e8
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-20 22:33:52 +1000

    Parse ‘i32’ as a type

--- src/type_.rs
+++ src/type_.rs
@@ -1,10 +1,11 @@
 use crate::ParserOutput;
 use dialect::{HighlightGroup, HighlightedSpan};
+use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::combinator::map;
 
 pub(crate) fn type_(s: &str) -> ParserOutput<'_> {
-    map(tag("u32"), |s| {
+    map(alt((tag("u32"), tag("i32"))), |s| {
         vec![HighlightedSpan {
             text: s,
             group: Some(HighlightGroup::PrimitiveTy),
@@ -24,6 +25,20 @@ mod tests {
                 "",
                 vec![HighlightedSpan {
                     text: "u32",
+                    group: Some(HighlightGroup::PrimitiveTy)
+                }]
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_i32() {
+        assert_eq!(
+            type_("i32"),
+            Ok((
+                "",
+                vec![HighlightedSpan {
+                    text: "i32",
                     group: Some(HighlightGroup::PrimitiveTy)
                 }]
             ))
