d1feb5c4f21e05e97bf906b63fd2497eb6bfff5f
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-26 13:24:33 +1000

    Add types to the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,6 +2,8 @@ use logos::Logos;
 
 #[derive(Debug, PartialEq, Logos)]
 enum Token {
+    #[regex("_?[A-Z][A-Za-z0-9]*")]
+    Type,
     #[error]
     #[regex("[ \t\n\r]", logos::skip)]
     Error,
@@ -34,5 +36,24 @@ mod tests {
     #[test]
     fn skips_carriage_returns() {
         assert_eq!(Token::lexer("\r\r").count(), 0);
+    }
+
+    #[test]
+    fn types_are_pascal_case() {
+        let mut lexer = Token::lexer("PascalCase123");
+        assert_eq!(lexer.next(), Some(Token::Type));
+        assert_eq!(lexer.slice(), "PascalCase123");
+    }
+
+    #[test]
+    fn types_cannot_start_with_number() {
+        assert_ne!(Token::lexer("123NotAType").next(), Some(Token::Type));
+    }
+
+    #[test]
+    fn types_can_start_with_an_underscore() {
+        let mut lexer = Token::lexer("_Unused123Type");
+        assert_eq!(lexer.next(), Some(Token::Type));
+        assert_eq!(lexer.slice(), "_Unused123Type");
     }
 }
