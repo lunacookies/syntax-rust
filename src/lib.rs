b22153a00f0fe15cf2379023b2411a026d73d2d7
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-26 11:54:48 +1000

    Add boilerplate for lexer

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,7 @@
 
 #![warn(missing_debug_implementations, rust_2018_idioms)]
 
-use nom::multi::many0;
-
-mod parser;
+mod lexer;
 
 #[derive(Debug)]
 pub struct RustHighlighter;
-
-type ParserOutput<'a> = nom::IResult<&'a str, Vec<dialect::HighlightedSpan<'a>>>;
-
-impl dialect::Highlight for RustHighlighter {
-    fn highlight<'input>(&self, input: &'input str) -> Vec<dialect::HighlightedSpan<'input>> {
-        match many0(parser::parse)(input) {
-            Ok(("", spans)) => spans.into_iter().flatten().collect(),
-            _ => vec![dialect::HighlightedSpan {
-                text: input,
-                group: None,
-            }],
-        }
-    }
-}
