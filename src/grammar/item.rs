9c27d8721ba26199f7874f272e317346fb6c383d
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-07-10 11:54:24 +1000

    Add error recovery to item parser

--- src/grammar/item.rs
+++ src/grammar/item.rs
@@ -3,30 +3,41 @@ mod struct_;
 mod trait_;
 
 use crate::Parser;
+use dialect::HighlightGroup;
 use fn_::parse_fn;
 use struct_::parse_struct;
 use trait_::parse_trait;
 
 pub(crate) fn parse_item(p: &mut Parser) {
-    if let Some(token) = p.peek() {
+    while let Some(token) = p.peek() {
         match token.kind {
-            crate::TokenKind::Fn => parse_fn(p),
-            crate::TokenKind::Struct => parse_struct(p),
-            crate::TokenKind::Trait => parse_trait(p),
+            crate::TokenKind::Fn => {
+                parse_fn(p);
+                break;
+            }
+            crate::TokenKind::Struct => {
+                parse_struct(p);
+                break;
+            }
+            crate::TokenKind::Trait => {
+                parse_trait(p);
+                break;
+            }
 
-            // parse_item should only be called if the next token has been verified to be able to
-            // start an item.
-            _ => panic!(),
+            crate::TokenKind::CloseBrace => {
+                break;
+            }
+            _ => p.eat(HighlightGroup::Error),
         }
     }
 }
 
-// These tests just check if each item’s parser has been hooked in correctly by looking at the first
-// HighlightedSpan found, rather than checking the entire parser output.
+// Most of these tests just check if each item’s parser has been hooked in correctly by looking at
+// the first HighlightedSpan found, rather than checking the entire parser output.
 #[cfg(test)]
 mod tests {
     use super::*;
-    use dialect::HighlightGroup;
+    use dialect::HighlightedSpan;
     use pretty_assertions::assert_eq;
 
     fn test(input: &str, expected_group: HighlightGroup) {
@@ -49,5 +60,49 @@ mod tests {
     #[test]
     fn parses_trait() {
         test("trait Marker {}", HighlightGroup::OtherKeyword);
+    }
+
+    #[test]
+    fn eats_errors_until_finds_valid_keyword() {
+        let mut parser = Parser::new("error blah fn");
+        parse_item(&mut parser);
+
+        assert_eq!(
+            parser.output,
+            vec![
+                HighlightedSpan {
+                    range: 0..5,
+                    group: HighlightGroup::Error,
+                },
+                HighlightedSpan {
+                    range: 6..10,
+                    group: HighlightGroup::Error,
+                },
+                HighlightedSpan {
+                    range: 11..13,
+                    group: HighlightGroup::OtherKeyword,
+                },
+            ],
+        );
+    }
+
+    #[test]
+    fn eats_errors_until_close_brace() {
+        let mut parser = Parser::new("invalid item }");
+        parse_item(&mut parser);
+
+        assert_eq!(
+            parser.output,
+            vec![
+                HighlightedSpan {
+                    range: 0..7,
+                    group: HighlightGroup::Error,
+                },
+                HighlightedSpan {
+                    range: 8..12,
+                    group: HighlightGroup::Error,
+                },
+            ],
+        );
     }
 }
